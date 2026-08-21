9e32dbfa7521a49d32169d57ee7d6afe2fb8bc87
JBYoussef <celiobofengola@gmail.com>
2026-08-21 15:08:56 +0100

    EX04 almost done\! Thanks Lord\!

--- java-00/ex04/Program.java
+++ java-00/ex04/Program.java
@@ -2,15 +2,45 @@ import java.util.Scanner;
 
 class   Program
 {
-    public static void  main( String[] args )
+    static void printRecursive( final char[] toplistNum, final char[] allOcurr )
     {
-        final Scanner   scanner = new Scanner();
+        int     greatest = 0;
+        int     occurrence;
+        int     result;
+        boolean[]  check = new boolean[10];
+
+        for (int k = 0; k < 10 && allOcurr[toplistNum[k]] != '\0'; k++)
+        {
+            occurrence = allOcurr[toplistNum[k]]; 
+            if (k == 0)
+                greatest = occurrence;
+
+            result = (occurrence * 10) / greatest;
+            if (result < (10 - k))
+            {
+                k = 0;
+                System.out.println("");
+                continue ;
+            }
+            if (!check[k])
+                System.out.print(occurrence + " ");
+            else
+                System.out.print("#");
+        }
+        for (int k = 0; k < 10 && allOcurr[toplistNum[k]] != '\0'; k++)
+            System.out.print(toplistNum[k] + " ");
+    }
+    public static void  main( final String[] args )
+    {
+        final Scanner   scanner = new Scanner(System.in);
         final char[]    allOcurr = new char[65535];
         final char[]    toplistNum = new char[10];
         char[]          iter;
         String          input;
+        char            prev_letter;
+        char            curr_letter;
         char            n_ocurr_on_lst;
-        char            letter_on_lst;
+        char            curr_letter_on_lst;
         char            n_ocurr_on_input;
         char            curr_letter_on_input;
 
@@ -20,7 +50,7 @@ class   Program
             input = scanner.nextLine();
             scanner.close();
             iter = input.toCharArray();
-            for (int i = 0; i < input.length(); i++)
+            for (int i = 0; i < iter.length; i++)
             {
                 curr_letter_on_input = iter[i];
                 n_ocurr_on_input = allOcurr[curr_letter_on_input];
@@ -29,19 +59,23 @@ class   Program
                 
                 for (int j = 0; j < 10; j++)
                 {
-                    n_ocurr_on_lst = allOcurr[toplistNum[j]];
-                    letter_on_lst = toplistNum[j];
-                    if (n_ocurr_on_input >= allOcurr[n_ocurr_on_lst])
+                    curr_letter_on_lst = toplistNum[j];
+                    n_ocurr_on_lst = allOcurr[curr_letter_on_lst];
+                    if (n_ocurr_on_input > n_ocurr_on_lst || (n_ocurr_on_input == n_ocurr_on_lst && curr_letter_on_input > curr_letter_on_lst))
                     {
-                        if (n_ocurr_on_input == allOcurr[n_ocurr_on_lst])
-                        for (int k = j; k < 10; k++)
+                        prev_letter = curr_letter_on_lst;
+                        for (int k = j + 1; k < 10 && allOcurr[toplistNum[k]] != '\0'; k++)
                         {
-
+                            curr_letter = toplistNum[k];
+                            toplistNum[k] = prev_letter;
+                            prev_letter = curr_letter;
                         }
+                        toplistNum[j] = curr_letter_on_input;
                     }
-
                 }
             }
+            System.out.println("");
+            printRecursive(toplistNum, allOcurr);
         }
         catch ( Exception e )
         {
